f8e8bd0ca54daf713123c95f24a486244d0179c9
Henry Lawrence <h.lawrence@windowslive.com>
2017-02-16 16:07:32 +1300

    Added files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// $ ( document ).ready(function() {
+// alert("dd");
+// });
+
+
+//
+
+
+//
+//
+// $( "#target" ).click(function() {
+//   alert( "Handler for .click() called." );
+// });
+
+
+  $(document).ready(function(){
+    if (jQuery) {
+      // jQuery is loaded
+      alert("Yeah!");
+    } else {
+      // jQuery is not loaded
+      alert("Doesn't Work");
+    }
+  });
